fddcf93a8b1c5b85a6c0199403f5999fcf780013
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-03-22 10:47:18 -0300

    fix: Login validation problem solution

--- client/src/Component/Home/Home-component.jsx
+++ client/src/Component/Home/Home-component.jsx
@@ -1,37 +1,48 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home-style.css'; // Importando o arquivo CSS para estilos
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import Header from '../Header/Header-component';
 
-
 function Main() {
   const navigate = useNavigate();
+  const [authenticated, setAuthenticated] = useState(false);
 
   useEffect(() => {
     // Configura o Axios para enviar cookies com todas as solicitações
     axios.defaults.withCredentials = true;
 
-    axios.get('https://system-sign.onrender.com/auth/verify')
-      .then(res => {
-        if (res.data.status === true) {
-          console.log('Usuário autenticado');
+    const checkAuthentication = async () => {
+      try {
+        const response = await axios.get("https://system-sign.onrender.com/auth/verify");
+        if (response.data.status === true) {
+          setAuthenticated(true);
+          console.log(response.data.message)
         } else {
-          console.log('Não autorizado');
           navigate('/');
         }
-      })
-      .catch(error => {
+      } catch (error) {
         console.error('Erro ao verificar a autenticação:', error);
         navigate('/');
-      });
-  }, []);
+      }
+    };
+
+    checkAuthentication();
+  }, [navigate]);
 
   return (
     <main className="home-container">
-      <Header/>
-      <h1>Welcome to Our Website</h1>
-      <p>This is a simple example of a Home page.</p>
+      <Header />
+      {authenticated ? (
+        <>
+          <h1>Welcome to Our Website</h1>
+          <p>This is a simple example of a Home page.</p>
+          <p>Autor: Lucas Pedro Fernandes. <a href="https://github.com/LucasPedro123">Veja meu GitHub</a></p>
+
+        </>
+      ) : (
+          <h1>Not Authorized</h1>
+      )}
     </main>
   );
 }
